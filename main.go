ae3db99165967f48f3f971af4ac4a5702f1b12f0
Yichun Ma <yicma@microsoft.com>
2022-07-15 21:45:33 +0800

    update

--- main.go
+++ main.go
@@ -5,18 +5,40 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"os"
+	"strings"
 )
 
 func main() {
-	f, err := os.ReadFile("D:\\terraform-test\\aks data references resource GH 13152 15557\\modules\\RoleAssignment\\main.tf")
+	diskFile, err := os.ReadFile("D:\\terraform-test\\aks data references resource GH 13152 15557\\modules\\RoleAssignment\\main.tf")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	hclFile, _ := hclsyntax.ParseConfig(diskFile, "main.tf", hcl.InitialPos)
+	body := hclFile.Body.(*hclsyntax.Body)
 
+	for _, block := range body.Blocks {
+		switch block.Type {
+		case "data", "resource":
+			for _, attribute := range block.Body.Attributes {
+				// TODO nested property
+				propertyName := attribute.Name
+				for _, variable := range attribute.Expr.Variables() {
+					if variable.RootName() == "var" {
+						variableName := variable[1].(hcl.TraverseAttr).Name
 
-	a, b := hclsyntax.ParseConfig(f, "main.tf", hcl.InitialPos)
-fmt.Println(a)
-	fmt.Println(b)
+						// Validate
+						fmt.Println(variableName, propertyName)
+						if strings.LastIndex(variableName, propertyName) == len(variableName) - len(propertyName) {
+							fmt.Println("valid")
+						} else {
+							fmt.Println("invalid")
+						}
+					}
+				}
+			}
+		default:
+		}
+	}
 }
